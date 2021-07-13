1bafd69edbb52df53cc8ec5799283e298ce50df5
Daniel J. Antonio <djedidiaha@gmail.com>
2021-07-13 08:29:59 +0700

    [main] add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	proseWrap: 'preserve',
+	printWidth: 150,
+	trailingComma: 'all',
+	tabWidth: 4,
+	semi: true,
+	singleQuote: true,
+	jsxBracketSameLine: true,
+	jsxSingleQuote: false,
+	useTabs: true,
+};
