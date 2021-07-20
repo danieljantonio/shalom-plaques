4d7c14ff1d9ade647007cb5165eab4d47a6a9078
Daniel J. Antonio <djedidiaha@gmail.com>
2021-07-20 12:31:20 +0700

    [main] Template tagline and features section

--- src/components/pages/Home/sections/features/features.section.tsx
+++ src/components/pages/Home/sections/features/features.section.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import ColumnHalf from '../../../../common/Column/half.column';
 import './features.section.scss';
 
 const FeaturesSection: React.FC = () => {
-	return <div className="row">Third section here</div>;
+	return (
+		<div className="features pad-y50-smo">
+			<div className="row container">
+				<ColumnHalf className="center-y hide-sm">
+					<div className="card mag-y50" style={{ height: '400px', width: '300px', backgroundColor: 'white' }}>
+						Image will be here
+					</div>
+				</ColumnHalf>
+				<ColumnHalf className="center-xy">
+					<div className="tag-header pad-y50-smo">
+						<h1 className="">High quality wood</h1>
+						<p>Bringing Word to the ends of the world</p>
+					</div>
+				</ColumnHalf>
+			</div>
+			<div className="row container">
+				<ColumnHalf className="center-xy">
+					<div className="tag-header pad-y50-smo">
+						<h1 className="">Shalom Plaques</h1>
+						<p>Bringing Word to the ends of the world</p>
+					</div>
+				</ColumnHalf>
+
+				<ColumnHalf className="center-y hide-sm">
+					<div className="card mag-y50" style={{ height: '400px', width: '300px', backgroundColor: 'white' }}>
+						Image will be here
+					</div>
+				</ColumnHalf>
+			</div>
+		</div>
+	);
 };
 
 export default FeaturesSection;
--- src/components/pages/Home/sections/tagline/tagline.section.tsx
+++ src/components/pages/Home/sections/tagline/tagline.section.tsx
@@ -4,15 +4,17 @@ import './tagline.section.scss';
 
 const TagLineSection: React.FC = () => {
 	return (
-		<div className="tagline row">
+		<div className="tagline row pad-y50-smo">
 			<ColumnHalf className="center-xy">
-				<div className="tag-header">
+				<div className="tag-header pad-y50-smo">
 					<h1>Shalom Plaques</h1>
 					<p>Bringing Word to the ends of the world</p>
 				</div>
 			</ColumnHalf>
-			<ColumnHalf>
-				<p>Tagline will be here</p>
+			<ColumnHalf className="center-xy hide-sm">
+				<div className="card mag-y50" style={{ height: '400px', width: '300px', backgroundColor: 'white' }}>
+					Image will be here
+				</div>
 			</ColumnHalf>
 		</div>
 	);
