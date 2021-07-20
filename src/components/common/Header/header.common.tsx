a616a5622e04dc846435f875a4259b99cd648aa4
Daniel J. Antonio <djedidiaha@gmail.com>
2021-07-20 12:28:57 +0700

    [main] Remove container from header

--- src/components/common/Header/header.common.tsx
+++ src/components/common/Header/header.common.tsx
@@ -5,37 +5,35 @@ import './header.common.scss';
 const Header: React.FC = () => {
 	return (
 		<nav className="navbar">
-			<div className="container">
-				<Link to="/" className="navbar-brand">
-					ShalomPlaques
-				</Link>
-				<div className="navigation">
-					<ul>
-						<li>
-							<NavLink exact to="/">
-								Home
-							</NavLink>
-						</li>
-						<li>
-							<NavLink exact to="/products">
-								Products
-							</NavLink>
-						</li>
-						<li>
-							<NavLink exact to="/about">
-								About Us
-							</NavLink>
-						</li>
-						<li>
-							<NavLink exact to="/contact">
-								Contact
-							</NavLink>
-						</li>
-						<li>
-							<button>Download Catalogue &gt;</button>
-						</li>
-					</ul>
-				</div>
+			<Link to="/" className="navbar-brand">
+				ShalomPlaques
+			</Link>
+			<div className="navigation">
+				<ul>
+					<li>
+						<NavLink exact to="/">
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink exact to="/products">
+							Products
+						</NavLink>
+					</li>
+					<li>
+						<NavLink exact to="/about">
+							About Us
+						</NavLink>
+					</li>
+					<li>
+						<NavLink exact to="/contact">
+							Contact
+						</NavLink>
+					</li>
+					<li>
+						<button>Download Catalogue &gt;</button>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
