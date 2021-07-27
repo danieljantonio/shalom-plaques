6738a1066df064547208dc5acfab324b5d233a69
Daniel J. Antonio <djedidiaha@gmail.com>
2021-07-27 13:23:43 +0700

    [main] Add sidenav and render product cards

--- src/components/pages/Products/products.page.tsx
+++ src/components/pages/Products/products.page.tsx
@@ -1,8 +1,51 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import ColumnThirds from '../../common/Column/third.column';
+import ProductCard from '../../common/Product/product-card.common';
 import './products.page.scss';
 
 const ProductsPage: React.FC = () => {
-	return <div>Products</div>;
+	const products = [
+		'Magnet',
+		'Frame',
+		'Box',
+		'Cross',
+		'Key Ring',
+		'Wall Plaque',
+		'Table Plaque',
+		'Church Supplies',
+		'Miscellaneous',
+		'New Products',
+	];
+	return (
+		<div className="product-page">
+			<h1>Products</h1>
+			<div className="row container">
+				<div className="sidenav">
+					<div className="card">
+						{products.map((product, index) => (
+							<NavLink key={index} exact to={`/products/${product.toLowerCase().replace(' ', '-')}`}>
+								{product}
+							</NavLink>
+						))}
+					</div>
+				</div>
+				<div className="main">
+					<div className="row ">
+						<ColumnThirds>
+							<ProductCard />
+						</ColumnThirds>
+						<ColumnThirds>
+							<ProductCard />
+						</ColumnThirds>
+						<ColumnThirds>
+							<ProductCard />
+						</ColumnThirds>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ProductsPage;
