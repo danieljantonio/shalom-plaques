1eb0e7110633b76b1df845ec71da5bc6e8b6eed7
Daniel J. Antonio <djedidiaha@gmail.com>
2021-07-29 16:47:13 +0700

    [main] fluid modal, set up mock data

--- src/components/common/ProductModal/product-modal.common.tsx
+++ src/components/common/ProductModal/product-modal.common.tsx
@@ -8,15 +8,25 @@ interface ModalProps {
 }
 
 // Modal.setAppElement('#root');
-
+const mockProductData = {
+	wording: 'God is Love',
+	code: 'AM-02',
+	description: 'Hand carved rectangular shape fridge magnet',
+	length: 6.5,
+	width: 5.5,
+	height: 0.5,
+	verse: 'Colossians 3:12',
+	price: 'RpXX,XXX',
+};
 const ReactModal: React.FC<ModalProps> = ({ openModal, setOpenModal }) => {
 	// const [isOpen, setIsOpen] = useState(false);
+	const productData = mockProductData;
 
 	return (
 		<div>
 			<button onClick={() => setOpenModal(true)}></button>
 			<Modal isOpen={openModal} onRequestClose={() => setOpenModal(false)} contentLabel="Example Modal">
-				Yay
+				Yay or Nay
 			</Modal>
 		</div>
 	);
