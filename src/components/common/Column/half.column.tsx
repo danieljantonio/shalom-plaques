8a59f153c7f45985e93ecb3dfb46b2de4c585b29
Daniel J. Antonio <djedidiaha@gmail.com>
2021-07-30 18:02:37 +0700

    [main] add styles to column

--- src/components/common/Column/half.column.tsx
+++ src/components/common/Column/half.column.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import './column.scss';
 import { ColumnProps } from './interfaces.column';
 
-const ColumnHalf: React.FC<ColumnProps> = ({ children, className = '' }) => {
-	return <div className={`col-half ${className}`}>{children}</div>;
+const ColumnHalf: React.FC<ColumnProps> = ({ children, className = '', style }) => {
+	return (
+		<div style={style} className={`col-half ${className}`}>
+			{children}
+		</div>
+	);
 };
 
 export default ColumnHalf;
--- src/components/common/Column/interfaces.column.tsx
+++ src/components/common/Column/interfaces.column.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+
 export interface ColumnProps {
 	className?: string;
+	style?: React.CSSProperties;
 }
