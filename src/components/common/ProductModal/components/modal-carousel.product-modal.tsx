6fcbab2a496f7608842cbd7099fab1a156ea9b37
Daniel J. Antonio <djedidiaha@gmail.com>
2021-07-30 16:32:49 +0700

    [main] Modal carousel

---
+++ src/components/common/ProductModal/components/modal-carousel.product-modal.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import './modal-carousel.product-modal.scss';
+
+const ModalCarousel: React.FC = () => {
+	const urls = [
+		'http://localhost:3000/sample-images/woodwork-test-1.jpeg',
+		'http://localhost:3000/sample-images/woodwork-test-2.jpeg',
+		'http://localhost:3000/sample-images/woodwork-test-3.jpeg',
+		'http://localhost:3000/sample-images/woodwork-test-4.jpeg',
+		'http://localhost:3000/sample-images/woodwork-test-5.jpeg',
+	];
+
+	return (
+		<Carousel
+			className="modal-carousel"
+			autoPlay={true}
+			emulateTouch={true}
+			showThumbs={false}
+			infiniteLoop={true}
+			showArrows={false}
+			showStatus={false}>
+			{urls.map((src, index) => (
+				<div className="carousel-container" key={index}>
+					<img className="bg-img" src={src} alt="test" />
+				</div>
+			))}
+		</Carousel>
+	);
+};
+
+export default ModalCarousel;
