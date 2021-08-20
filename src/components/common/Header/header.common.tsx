5bca7a9685a5e80de66a51e665695b9ae95710fd
Daniel J. Antonio <djedidiaha@gmail.com>
2021-08-20 17:27:32 +0700

    [main] Fix mobile navbar

--- src/components/common/Header/header.common.tsx
+++ src/components/common/Header/header.common.tsx
@@ -2,35 +2,37 @@ import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './header.common.scss';
 
-const NavigationComponents = () => (
-	<div className="navigation">
-		<ul>
-			<li>
-				<NavLink exact to="/">
-					Home
-				</NavLink>
-			</li>
-			<li>
-				<NavLink exact to="/products">
-					Products
-				</NavLink>
-			</li>
-			<li>
-				<NavLink exact to="/about">
-					About Us
-				</NavLink>
-			</li>
-			<li>
-				<NavLink exact to="/contact">
-					Contact
-				</NavLink>
-			</li>
-			<li>
-				<button onClick={() => window.open('/shalom-handicraft-catalogue.pdf')}>Download Catalogue &gt;</button>
-			</li>
-		</ul>
-	</div>
-);
+const NavigationComponents: React.FC<{ expand: boolean }> = ({ expand }) => {
+	return (
+		<div className={`navigation${expand ? ' active' : ''}`}>
+			<ul>
+				<li>
+					<NavLink exact to="/">
+						Home
+					</NavLink>
+				</li>
+				<li>
+					<NavLink exact to="/products">
+						Products
+					</NavLink>
+				</li>
+				<li>
+					<NavLink exact to="/about">
+						About Us
+					</NavLink>
+				</li>
+				<li>
+					<NavLink exact to="/contact">
+						Contact
+					</NavLink>
+				</li>
+				<li>
+					<button onClick={() => window.open('/shalom-handicraft-catalogue.pdf')}>Download Catalogue &gt;</button>
+				</li>
+			</ul>
+		</div>
+	);
+};
 
 const Header: React.FC = () => {
 	const [expand, setExpand] = useState<boolean>(false);
@@ -46,7 +48,7 @@ const Header: React.FC = () => {
 					<div id="burger-3" className="burger-icon"></div>
 				</div>
 			</button>
-			{window.screen.width >= 800 ? <NavigationComponents /> : expand ? <NavigationComponents /> : <></>}
+			<NavigationComponents expand={expand} />
 		</nav>
 	);
 };
