df574a3171239cb623b8d6d4549c23abab868521
Daniel J. Antonio <djedidiaha@gmail.com>
2021-08-20 16:27:50 +0700

    [main] footer done

--- src/components/common/Footer/footer.common.tsx
+++ src/components/common/Footer/footer.common.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import ColumnHalf from '../Column/half.column';
 import './footer.common.scss';
 
-const Footer: React.FC = () => {
-	return <div>Footer</div>;
-};
+const Footer: React.FC = () => (
+	<div className="footer">
+		<ColumnHalf className="footer-left">
+			<h2>Shalom Handicrafts</h2>
+		</ColumnHalf>
+		<ColumnHalf className="footer-right">
+			<a href="/">Instagram</a>
+			<a href="/">Tokopedia</a>
+			<a href="/">Whatsapp</a>
+		</ColumnHalf>
+	</div>
+);
 
 export default Footer;
