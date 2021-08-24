22a658ad6de1f657124b147cb2730c5339c68105
Daniel J. Antonio <djedidiaha@gmail.com>
2021-08-24 16:30:52 +0700

    [main] about us text and css

--- src/components/pages/AboutUs/aboutus.page.tsx
+++ src/components/pages/AboutUs/aboutus.page.tsx
@@ -7,33 +7,39 @@ const AboutUsPage: React.FC = () => {
 	return (
 		<div className="about-us">
 			<div className="container center-xy about-1">
-				<h1>Products</h1>
-				<p>
-					Founded by Bob Ross in 1978, Shalom Plaques and Handicrafts has been a favored crafter for a variety of woodcrafts. The company is
-					now managed by Kennise Y. S. and has continued to flourish over the years.
+				<h1>About Us</h1>
+				<p style={{ width: '90%' }}>
+					For over 30 years, Shalom Handicrafts have produced high quality products made from locally selected woods. Exporting Indonesian
+					woodwork to over 23 countries all around the world and providing training and jobs to less fortunate individuals.
 				</p>
 			</div>
 
 			<div className="row container">
 				<ColumnHalf className="center-xy">
 					<div className="tag-header pad-y50-smo">
-						<h1 className="">Shalom Plaques</h1>
-						<p>Founded in 1978</p>
+						{/* <h2 className="">Shalom Plaques</h2> */}
+						<p>
+							Enabling people of lower-class to provide for their families through permanent employment. Many of which have been with us
+							for over 25 years.
+						</p>
 					</div>
 				</ColumnHalf>
 
 				<ColumnHalf className="center-y hide-sm pv-lg-10p">
-					<img src={`${baseUrl}/sample-images/woodwork-test-3.jpeg`} alt="img" className="card mag-y50" />
+					<img src={`${baseUrl}/production/working-4.JPG`} alt="img" className="card mag-y50" />
 				</ColumnHalf>
 			</div>
 			<div className="row container">
 				<ColumnHalf className="center-y hide-sm pv-lg-10p">
-					<img src={`${baseUrl}/sample-images/woodwork-test-1.jpeg`} alt="img" className="card mag-y50" />
+					<div className="img-container card mag-y50">
+						<img src={`${baseUrl}/production/cnc-machine.JPG`} alt="img" className="" />
+						<div className="darken"></div>
+					</div>
 				</ColumnHalf>
 				<ColumnHalf className="center-xy">
 					<div className="tag-header pad-y50-smo">
-						<h1 className="">High quality wood</h1>
-						<p>Bringing Word to the ends of the world</p>
+						{/* <h2 className="">High quality wood</h2> */}
+						<p>Training them to be proficient in operating the machines and techologies of the company.</p>
 					</div>
 				</ColumnHalf>
 			</div>
