d75bfdea3226c23ce065e6a0e593a13be7c35fb8
Daniel J. Antonio <djedidiaha@gmail.com>
2021-08-26 15:21:49 +0700

    [main] Fix features padding

--- src/components/pages/Home/sections/features/features.section.tsx
+++ src/components/pages/Home/sections/features/features.section.tsx
@@ -7,11 +7,11 @@ const FeaturesSection: React.FC = () => {
 	return (
 		<div className="features pad-y50-smo">
 			<div className="row container">
-				<ColumnHalf className="center-y hide-sm pv-lg-10p">
+				<ColumnHalf className="center-y hide-sm">
 					<img src={`${baseUrl}/sample-images/woodwork-test-1.jpeg`} alt="img" className="card mag-y50" />
 				</ColumnHalf>
 				<ColumnHalf className="center-xy">
-					<div className="tag-header pad-y50-smo">
+					<div className="tag-header">
 						<h1 className="">High quality wood</h1>
 						<h2>Bringing Word to the ends of the world</h2>
 					</div>
@@ -19,13 +19,13 @@ const FeaturesSection: React.FC = () => {
 			</div>
 			<div className="row container">
 				<ColumnHalf className="center-xy">
-					<div className="tag-header pad-y50-smo">
+					<div className="tag-header">
 						<h1 className="">Shalom Plaques</h1>
 						<h2>Bringing Word to the ends of the world</h2>
 					</div>
 				</ColumnHalf>
 
-				<ColumnHalf className="center-y hide-sm pv-lg-10p">
+				<ColumnHalf className="center-y hide-sm">
 					<img src={`${baseUrl}/sample-images/woodwork-test-3.jpeg`} alt="img" className="card mag-y50" />
 				</ColumnHalf>
 			</div>
