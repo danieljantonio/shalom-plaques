9af6b0725ff75cffa3c0ef70fc08efd93612c1ff
Daniel J. Antonio <djedidiaha@gmail.com>
2021-08-26 14:42:26 +0700

    [main] footer link changes

--- src/components/common/Footer/footer.common.tsx
+++ src/components/common/Footer/footer.common.tsx
@@ -8,9 +8,18 @@ const Footer: React.FC = () => (
 			<h2>Shalom Handicrafts</h2>
 		</ColumnHalf>
 		<ColumnHalf className="footer-right">
-			<a href="/">Instagram</a>
-			<a href="/">Tokopedia</a>
-			<a href="/">Whatsapp</a>
+			<a href="https://instagram.com/maranathahandicraft" target="_blank" rel="noreferrer">
+				Instagram
+			</a>
+			<a href="https://tokopedia.link/GHd4ZyWiQib" target="_blank" rel="noreferrer">
+				Tokopedia
+			</a>
+			<a
+				href="https://wa.me/6282311135240?text=Hi%2C%20I%20would%20like%20to%20inquire%20about%20your%20products"
+				target="_blank"
+				rel="noreferrer">
+				Whatsapp
+			</a>
 		</ColumnHalf>
 	</div>
 );
