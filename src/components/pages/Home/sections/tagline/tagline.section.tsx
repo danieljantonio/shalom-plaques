c53eb8c6bd26b0d20ac05b70951a0227adc6b4da
Daniel J. Antonio <djedidiaha@gmail.com>
2021-08-26 15:22:00 +0700

    [main] tagline view products button

--- src/components/pages/Home/sections/tagline/tagline.section.tsx
+++ src/components/pages/Home/sections/tagline/tagline.section.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { baseUrl } from '../../../../../helpers/helpers';
 import ColumnHalf from '../../../../common/Column/half.column';
 import './tagline.section.scss';
@@ -10,6 +11,9 @@ const TagLineSection: React.FC = () => {
 				<div className="tag-header pad-y50-smo">
 					<h1>Shalom Plaques</h1>
 					<p>Bringing the Word to the ends of the world</p>
+					<Link to="/products" className="view-products">
+						View Products
+					</Link>
 				</div>
 			</ColumnHalf>
 			<ColumnHalf className="center-xy hide-sm">
