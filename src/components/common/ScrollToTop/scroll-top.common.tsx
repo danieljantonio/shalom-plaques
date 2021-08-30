5d46d60f1018544c16ab99b0cd28b8f378675375
Daniel J. Antonio <djedidiaha@gmail.com>
2021-08-30 17:23:12 +0700

    [main] reuse scroll to top as bottom util

--- src/components/common/ScrollToTop/scroll-top.common.tsx
+++ src/components/common/ScrollToTop/scroll-top.common.tsx
@@ -4,8 +4,22 @@ import './scroll-top.common.scss';
 
 const ScrollToTop: React.FC = () => {
 	return (
-		<div className="scroll-to-top" onClick={() => window.scrollTo(0, 0)}>
-			<img src={`${baseUrl}/icons/arrow-up.svg`} alt="scroll to top" />
+		<div className="bottom-utils">
+			<div className="util-icon" onClick={() => window.scrollTo(0, 0)}>
+				<img className="large-util" src={`${baseUrl}/icons/tokopedia-icon.svg`} alt="scroll to top" />
+			</div>
+			<div className="util-icon" onClick={() => window.scrollTo(0, 0)}>
+				<img className="medium-util" src={`${baseUrl}/icons/whatsapp-icon.svg`} alt="scroll to top" />
+			</div>
+			<div className="util-icon" onClick={() => window.scrollTo(0, 0)}>
+				<img className="medium-util" src={`${baseUrl}/icons/instagram-icon.svg`} alt="scroll to top" />
+			</div>
+			<div className="util-icon" onClick={() => window.scrollTo(0, 0)}>
+				<img className="medium-util" src={`${baseUrl}/icons/email-icon.svg`} alt="scroll to top" />
+			</div>
+			<div className="util-icon" onClick={() => window.scrollTo(0, 0)}>
+				<img src={`${baseUrl}/icons/arrow-up.svg`} alt="scroll to top" />
+			</div>
 		</div>
 	);
 };
