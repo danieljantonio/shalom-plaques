6e8e0b5e71308a9b023039c90cbd77a6d947299b
Daniel J. Antonio <djedidiaha@gmail.com>
2021-08-31 13:42:53 +0700

    [main] scroll to top + other icons

--- src/components/common/ScrollToTop/scroll-top.common.tsx
+++ src/components/common/ScrollToTop/scroll-top.common.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { baseUrl } from '../../../helpers/helpers';
+import { baseUrl, socialLinks } from '../../../helpers/helpers';
 import './scroll-top.common.scss';
 
 const ScrollToTop: React.FC = () => {
 	return (
 		<div className="bottom-utils">
-			<div className="util-icon" onClick={() => window.scrollTo(0, 0)}>
+			<div className="util-icon" onClick={() => window.open(socialLinks.tokopedia, "_blank")}>
 				<img className="large-util" src={`${baseUrl}/icons/tokopedia-icon.svg`} alt="scroll to top" />
 			</div>
-			<div className="util-icon" onClick={() => window.scrollTo(0, 0)}>
+			<div className="util-icon" onClick={() => window.open(socialLinks.whatsapp, "_blank")}>
 				<img className="medium-util" src={`${baseUrl}/icons/whatsapp-icon.svg`} alt="scroll to top" />
 			</div>
-			<div className="util-icon" onClick={() => window.scrollTo(0, 0)}>
+			<div className="util-icon" onClick={() => window.open(socialLinks.instagram, "_blank")}>
 				<img className="medium-util" src={`${baseUrl}/icons/instagram-icon.svg`} alt="scroll to top" />
 			</div>
-			<div className="util-icon" onClick={() => window.scrollTo(0, 0)}>
+			<div className="util-icon" onClick={() => window.location.href = socialLinks.email}>
 				<img className="medium-util" src={`${baseUrl}/icons/email-icon.svg`} alt="scroll to top" />
 			</div>
 			<div className="util-icon" onClick={() => window.scrollTo(0, 0)}>
