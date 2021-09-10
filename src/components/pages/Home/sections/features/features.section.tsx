ef040479855aacdb04f26127417358a5b56b881e
Daniel J. Antonio <djedidiaha@gmail.com>
2021-09-10 22:16:53 +0700

    [main] replace arrows with text

--- src/components/pages/Home/sections/features/features.section.tsx
+++ src/components/pages/Home/sections/features/features.section.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { baseUrl } from '../../../../../helpers/helpers';
+import ColumnThird from '../../../../common/Column/half.column';
 import div from '../../../../common/Column/third.column';
 import './features.section.scss';
 
 const FeaturesSection: React.FC = () => {
 	return (
-		<div className="features pad-y50-smo">
-			<div className="production">
+		<div className="container features pad-y50-smo">
+			<ColumnThird className="production hide-sm">
 				<img src={`${baseUrl}/production/material-wood-2.JPG`} alt="img" className="card" />
-			</div>
-			<div className="arrows">
-				<img src={`${baseUrl}/icons/arrow-diagonal-black.png`} alt="arrow" className="arrow-icon" />
-				<img src={`${baseUrl}/icons/arrow-diagonal-black.png`} alt="arrow" className="arrow-icon reverse" />
-			</div>
-			<div className="production">
+			</ColumnThird>
+			<ColumnThird className="production">
+				<h1>High Quality Handicrafts</h1>
+			</ColumnThird>
+			<ColumnThird className="production hide-sm">
 				<img src={`${baseUrl}/product-banner/product-banner-2.png`} alt="img" className="card" />
-			</div>
+			</ColumnThird>
 		</div>
 	);
 };
