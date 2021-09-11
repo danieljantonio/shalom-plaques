5154220791269849b78448f0a3203bc23ffb5f55
Daniel Antonio <djedidiaha@gmail.com>
2021-09-11 16:06:05 +0700

    [main] get subcategory on click, partially working

--- src/components/pages/Products/components/side-nav.products.tsx
+++ src/components/pages/Products/components/side-nav.products.tsx
@@ -6,32 +6,41 @@ interface Props {
 	productCategories: any
 }
 
-const SideNav: React.FC<Props> = ({productCategories}) => {
+const SideNav: React.FC<Props> = ({productCategories: categories}) => {
+	const productCategories = Object.keys(categories)
 	const [isExpand, setExpand] = useState<boolean[]>(productCategories.map((category: any) => false));
 	const [update, setUpdated] = useState<boolean>(false);
+	const [subCategories, setSubCategories] = useState<any>([])
 
 	useEffect(() => {}, [update]);
 	
 	const updateStatus = (index: number): void => {
-		console.log(`click ${index}: ${isExpand[index]}`);
+		// console.log(`click ${index}: ${isExpand[index]}`);
 		const expand = isExpand;
 		expand[index] = !isExpand[index];
 		setExpand(expand);
 		setUpdated(!update);
+		setSubCategories(Object.values(categories)[index])
 	}
 					
 
 	return (
-	<div className="sidenav">
-		<div className="card">
-			{productCategories.map((product: any, index: any) => (
-				<div className="header" onClick={() => updateStatus(index)}>
-					<p>{product}</p>
-					<img src={`/icons/icon_${isExpand[index] ? 'minus' : 'plus'}.svg`} alt="expand" />
-				</div>
-			))}
+		<div className="sidenav">
+			<div className="card">
+				{productCategories.map((product: any, index: any) => (
+					<div className="sidenav-item">
+						<div className="header" onClick={() => updateStatus(index)}>
+							<p>{product}</p>
+							<img src={`/icons/icon_${isExpand[index] ? 'minus' : 'plus'}.svg`} alt="expand" />
+						</div>
+						<div className="content">
+							{subCategories.map((subCategory: string) => <p>{subCategory}</p>)}
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
-	</div>)
+	)
 };
 
 export default SideNav;
