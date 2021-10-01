c8e02156a6fdc90dea1e939eed309eba8546231e
Daniel J. Antonio <djedidiaha@gmail.com>
2021-10-01 15:01:34 +0700

    [main] add carousell to about us

--- src/components/pages/AboutUs/aboutus.page.tsx
+++ src/components/pages/AboutUs/aboutus.page.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
 import { baseUrl } from '../../../helpers/helpers';
 import ColumnHalf from '../../common/Column/half.column';
+import CarouselSection from '../Home/sections/carousel/carousel.section';
 import './aboutus.page.scss';
 
 const AboutUsPage: React.FC = () => {
+	const urls = [
+		`${baseUrl}/production/people-1.JPG`,
+		`${baseUrl}/production/people-2.JPG`,
+		`${baseUrl}/production/people-3.JPG`,
+		`${baseUrl}/production/people-4.JPG`,
+		`${baseUrl}/production/people-5.JPG`,
+		`${baseUrl}/production/people-6.JPG`,
+		`${baseUrl}/production/people-7.JPG`,
+		`${baseUrl}/production/people-8.JPG`,
+	];
 	return (
 		<div className="about-us">
 			<div className="container center-xy about-1">
@@ -22,26 +34,17 @@ const AboutUsPage: React.FC = () => {
 							Enabling people of lower-class to provide for their families through permanent employment. Many of which have been with us
 							for over 25 years.
 						</p>
+						<p>Training them to be proficient in operating the machines and techologies of the company.</p>
 					</div>
 				</ColumnHalf>
 
 				<ColumnHalf className="center-y hide-sm pv-lg-10p">
-					<img src={`${baseUrl}/production/working-4.JPG`} alt="img" className="card mag-y50" />
+					<img src={`${baseUrl}/production/people-6.JPG`} alt="img" className="card mag-y50 img-flip" />
 				</ColumnHalf>
 			</div>
-			<div className="row container">
-				<ColumnHalf className="center-y hide-sm pv-lg-10p">
-					<div className="img-container card mag-y50">
-						<img src={`${baseUrl}/production/cnc-machine.JPG`} alt="img" className="" />
-						<div className="darken"></div>
-					</div>
-				</ColumnHalf>
-				<ColumnHalf className="center-xy">
-					<div className="tag-header pad-y50-smo">
-						{/* <h2 className="">High quality wood</h2> */}
-						<p>Training them to be proficient in operating the machines and techologies of the company.</p>
-					</div>
-				</ColumnHalf>
+			<div>
+				<h1>Meet our Workers</h1>
+				<CarouselSection urls={urls} />
 			</div>
 		</div>
 	);
