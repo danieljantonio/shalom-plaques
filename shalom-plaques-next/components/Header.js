73f75dff6edeaebaac56ce51bb41a347be478b17
joelelo <elioenaijoel@gmail.com>
2022-07-23 22:39:48 +0700

    header

--- shalom-plaques-next/components/Header.js
+++ shalom-plaques-next/components/Header.js
@@ -1,7 +1,48 @@
-import React from 'react';
-
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { MdKeyboardArrowLeft } from 'react-icons/md';
 const Header = () => {
-	return <div>Header</div>;
+	const [open, setOpen] = useState(false);
+	const handleMenu = () => {
+		const prev = open;
+		setOpen(!prev);
+	};
+	return (
+		<div className="flex flex-col">
+			<div className="fixed flex justify-between shadow-sm w-full h-fit">
+				<div className="w-2/3 p-2">
+					<Image src="/icons/shalom-brand.png" layout="responsive" width={1059} height={239.25} />
+				</div>
+
+				<MdKeyboardArrowLeft
+					onClick={handleMenu}
+					className={
+						open
+							? 'w-15% h-fit aspect-square text-amber-800 -rotate-90 transition-transform my-auto mr-1'
+							: 'w-15% h-fit aspect-square text-amber-800 transition-transform my-auto mr-1'
+					}
+				/>
+			</div>
+			<div className={open ? 'flex flex-col text-center shadow-sm text-amber-800 text-xl' : 'scale-0 '}>
+				<Link href="/">
+					<a>Home</a>
+				</Link>
+				<Link href="/#products">
+					<a>Products</a>
+				</Link>
+				<Link href="/#about">
+					<a>About Us</a>
+				</Link>
+				<Link href="/#contact">
+					<a>Contact Us</a>
+				</Link>
+				<Link href="/#download">
+					<a>Download Catalogue</a>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
--- shalom-plaques-next/tailwind.config.js
+++ shalom-plaques-next/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			spacing: {
+				'15%': '12%',
+			},
+		},
 	},
 	plugins: [],
 };
