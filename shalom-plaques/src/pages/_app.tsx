1766f431df09f8ef178d81bff2d606c9dec3fa49
joelelo <elioenaijoel@gmail.com>
2022-07-25 10:03:28 +0700

    icon

--- shalom-plaques/src/pages/_app.tsx
+++ shalom-plaques/src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<link rel="icon" href="/icons/shalom-favicon3.png" type="image/png" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
