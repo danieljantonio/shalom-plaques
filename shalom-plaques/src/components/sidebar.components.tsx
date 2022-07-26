83afd44eff4288fa44eded3f08885c5187787624
joelelo <elioenaijoel@gmail.com>
2022-07-26 21:09:46 +0700

    fix position and margin sidebar

--- shalom-plaques/src/components/sidebar.components.tsx
+++ shalom-plaques/src/components/sidebar.components.tsx
@@ -15,15 +15,15 @@ const Sidebar = ({ categories }: any) => {
 	return (
 		<div className='flex flex-col w-80 shadow-md min-h-full mb-0'>
 			{/* Search */}
-			<div className='relative px-5 mt-5 grow-0 shrink basis-auto'>
-				<AiOutlineSearch className='absolute top-1/2 -translate-y-1/2 left-8 h-5 w-5' />
+			<div className='relative px-5 my-5 grow-0 shrink basis-auto'>
+				<AiOutlineSearch className='absolute top-1/2 -translate-y-1/2 left-8 h-5 w-5 cursor-text' />
 				<input type='search' className='input w-full input-bordered input-sm pl-9' placeholder='Search...' />
 			</div>
 			<div className='grow shrink basis-auto'>
 				{/* Checkbox */}
 				{categories.map((category: ICategory, i: number) => {
 					return (
-						<label key={i} className='flex justify-between px-2 py-1'>
+						<label key={i} className='flex justify-between px-2 py-1 cursor-pointer'>
 							<p className='text-lg'>{category.name}</p>
 							<input type='checkbox' onClick={(e) => changeState(i, e.currentTarget.checked)} />
 						</label>
@@ -31,7 +31,7 @@ const Sidebar = ({ categories }: any) => {
 				})}
 			</div>
 			<Link href='/#'>
-				<a className='grow-0 shrink auto'>back to top</a>
+				<a className='grow-0 shrink auto text-center'>back to top</a>
 			</Link>
 		</div>
 	);
