d71a2a98f79aa8b4b27b566ad890ef4860cc011a
joelelo <elioenaijoel@gmail.com>
2022-07-26 20:00:16 +0700

    Searchbar react-icons

--- shalom-plaques/src/components/sidebar.components.tsx
+++ shalom-plaques/src/components/sidebar.components.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { AiOutlineSearch } from 'react-icons/ai';
 const Sidebar = ({ categories }: any) => {
 	const [checks, setChecks] = useState(
 		categories.map(() => {
@@ -13,6 +13,12 @@ const Sidebar = ({ categories }: any) => {
 	};
 	return (
 		<div className='w-80'>
+			{/* Search */}
+			<div className='relative px-5 mt-5'>
+				<AiOutlineSearch className='absolute top-1/2 -translate-y-1/2 left-8 h-5 w-5' />
+				<input type='search' className='input w-full input-bordered input-sm pl-9' placeholder='Search...' />
+			</div>
+			{/* Checkbox */}
 			{categories.map((category: ICategory, i: number) => {
 				return (
 					<label key={i} className='flex justify-between px-2 py-1'>
