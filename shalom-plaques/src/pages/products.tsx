3665a582c8b179500ff4cf3b3d7294c441914ba6
Daniel Antonio <djedidiaha@gmail.com>
2022-07-29 01:41:37 +0700

    attempt to create product sider. install formkit autoanimate to manually create accordion

--- shalom-plaques/src/pages/products.tsx
+++ shalom-plaques/src/pages/products.tsx
@@ -1,8 +1,47 @@
-import { NextPage } from 'next';
+import { GetStaticProps, NextPage } from 'next';
 import React from 'react';
 
-const Products: NextPage = () => {
-	return <div>Product Catalogue</div>;
+type Props = {
+	categories: ICategory[];
+	subCategories: ISubCategory[];
+	products: IProduct[];
+};
+
+const Products: NextPage<Props> = ({ categories, subCategories, products }) => {
+	return (
+		<>
+			<div>
+				<div>Categories</div>
+				{categories.map((category) => (
+					<div className='collapse border border-black mb-1'>
+						<input type='checkbox' className='peer' />
+						<div className='collapse-title bg-secondary text-primary'>{category.name}</div>
+						<div className='collapse-content bg-secondary text-primary'>
+							{category.subCategories.map((subCategory) => (
+								<div className='p-2 hover:bg-primary'>
+									<p>{subCategory.name}</p>
+								</div>
+							))}
+						</div>
+					</div>
+				))}
+			</div>
+		</>
+	);
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+	const { categories } = await fetch('http://localhost:5000/category').then((data) => data.json());
+	const { subCategories } = await fetch('http://localhost:5000/subcategory').then((data) => data.json());
+	const { products } = await fetch('http://localhost:5000/product').then((data) => data.json());
+
+	return {
+		props: {
+			categories,
+			subCategories,
+			products,
+		},
+	};
 };
 
 export default Products;
