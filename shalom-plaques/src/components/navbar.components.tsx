c247a76dd67c0e3602ee72ada2cee630a036377d
Daniel Antonio <djedidiaha@gmail.com>
2022-07-28 10:08:01 +0700

    set links on header

--- shalom-plaques/src/components/navbar.components.tsx
+++ shalom-plaques/src/components/navbar.components.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 const Navbar = () => {
 	return (
 		<div className='navbar bg-base-100 shadow-md z-50'>
 			<div className='navbar-start'>
-				<div className='h-11 aspect-logo'>
-					<Image src='/icons/shalom-brand.png' width={4236} height={957} layout='responsive' />
-				</div>
+				<Link href='/'>
+					<div className='h-11 aspect-logo hover:cursor-pointer'>
+						<Image src='/icons/shalom-brand.png' width={4236} height={957} layout='responsive' />
+					</div>
+				</Link>
 			</div>
-			<div className='navbar-center'>
-				<a className='btn btn-ghost normal-case text-xl text-primary'>daisyUI</a>
+			<Link href='/' className='navbar-center'>
+				<a className='btn btn-ghost normal-case text-xl text-primary'>Shalom Plaques</a>
+			</Link>
+
+			<div className='navbar-end'>
+				<Link href='/products' className='navbar-end'>
+					<a className='btn btn-ghost normal-case text-xl text-primary'>Products</a>
+				</Link>
+				<Link href='/about-us' className='navbar-end'>
+					<a className='btn btn-ghost normal-case text-xl text-primary'>About Us</a>
+				</Link>
+				<Link href='/contact-us' className='navbar-end'>
+					<a className='btn btn-ghost normal-case text-xl text-primary'>Contact Us</a>
+				</Link>
 			</div>
-			<div className='navbar-end'>Test</div>
 		</div>
 	);
 };
