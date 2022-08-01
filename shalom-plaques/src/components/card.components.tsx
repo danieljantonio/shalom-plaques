0b9102d2c67a5c4464816feb2f29c17e012f50c3
Daniel Antonio <djedidiaha@gmail.com>
2022-08-01 17:49:32 +0700

    fix card renders at small size before image loads

--- shalom-plaques/src/components/card.components.tsx
+++ shalom-plaques/src/components/card.components.tsx
@@ -1,17 +1,17 @@
-import Link from 'next/link';
 import React from 'react';
+
 const Card = ({ product }: { product: IProduct }) => {
 	return (
-		<div className="card w-96 bg-base-100 shadow-lg">
+		<div className='card m-4 mx-auto w-96 bg-base-100 shadow-lg'>
 			<figure>
-				<img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+				<img src='https://placeimg.com/400/225/arch' className='h-56' alt='Shoes' />
 			</figure>
-			<div className="card-body">
-				<h2 className="card-title">{product.series}</h2>
+			<div className='card-body'>
+				<h2 className='card-title'>{product.series}</h2>
 				<p>{product.description}</p>
-				<div className="card-actions justify-end">
-					<div className="badge badge-outline">{product.category.name}</div>
-					<div className="badge badge-outline">{product.subCategory.name}</div>
+				<div className='card-actions justify-end'>
+					<div className='badge badge-outline'>{product.category.name}</div>
+					<div className='badge badge-outline'>{product.subCategory.name}</div>
 				</div>
 			</div>
 		</div>
