e4a3ce2a3a9b927239fc145508c72e30b3ba03d8
joelelo <elioenaijoel@gmail.com>
2022-08-01 16:11:47 +0700

    sidebar w/ category +sub

--- shalom-plaques/src/components/sidebar.components.tsx
+++ shalom-plaques/src/components/sidebar.components.tsx
@@ -1,17 +1,72 @@
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
-const Sidebar = ({ categories }: any) => {
-	const [checks, setChecks] = useState(
-		categories.map(() => {
-			return false;
+type Props = {
+	categories: ICategory[];
+};
+
+const Sidebar = ({ categories }: Props) => {
+	const [checks, setChecks] = useState<boolean[][]>(
+		categories.map((category) => {
+			return category.subCategories.map(() => {
+				return false;
+			});
 		})
 	);
-	const changeState = (index: number, checked: boolean) => {
-		let temp = [...checks];
-		temp[index] = checked;
-		setChecks(temp);
+	const changeState = (index: number, index2: number, checked: boolean) => {
+		console.log(index, index2, checked);
+		const temp = [...checks[index]];
+		setChecks(
+			categories.map((category, i) => {
+				if (i === index) {
+					return category.subCategories.map((sub, j) => {
+						if (j === index2) {
+							return checked;
+						}
+						return temp[j];
+					});
+				}
+				return category.subCategories.map(() => {
+					return false;
+				});
+			})
+		);
 	};
+	const handleReset = () => {
+		setChecks(
+			categories.map((category) => {
+				return category.subCategories.map(() => {
+					return false;
+				});
+			})
+		);
+	};
+	const handleClickCategory = (index: number) => {
+		setChecks(
+			categories.map((category, i) => {
+				if (i === index) {
+					return category.subCategories.map(() => {
+						return true;
+					});
+				}
+				return category.subCategories.map(() => {
+					return false;
+				});
+			})
+		);
+	};
+	useEffect(() => {
+		setChecks(
+			categories.map((category) => {
+				return category.subCategories.map(() => {
+					return false;
+				});
+			})
+		);
+	}, []);
+	useEffect(() => {
+		console.log(checks);
+	}, [checks]);
 	return (
 		<div className='flex flex-col w-80 shadow-md min-h-full mb-0'>
 			{/* Search */}
@@ -20,15 +75,21 @@ const Sidebar = ({ categories }: any) => {
 				<input type='search' className='input w-full input-bordered input-sm pl-9' placeholder='Search...' />
 			</div>
 			{/* Checkbox */}
+			<button onClick={handleReset}>Reset</button>
 			<div className='grow shrink basis-auto'>
-				{categories.map((category: ICategory, i: number) => {
-					return (
-						<label key={i} className='flex justify-between px-2 py-1 cursor-pointer btn-ghost'>
-							<p className='text-lg'>{category.name}</p>
-							<input type='checkbox' onClick={(e) => changeState(i, e.currentTarget.checked)} />
-						</label>
-					);
-				})}
+				{categories.map((category, i) => (
+					<div key={category._id} className='flex flex-col justify-between px-2 py-1 cursor-pointer'>
+						<p onClick={() => handleClickCategory(i)} className='text-lg btn-ghost px-2'>
+							{category.name}
+						</p>
+						{category.subCategories.map((subCategory, j) => (
+							<label className='ml-3 btn-ghost flex justify-between px-2 cursor-pointer'>
+								<p className='text-lg'>{subCategory.name}</p>
+								<input type='checkbox' checked={checks[i][j]} onClick={(e) => changeState(i, j, e.currentTarget.checked)} />
+							</label>
+						))}
+					</div>
+				))}
 			</div>
 			{/* footer */}
 			<Link href='/#'>
