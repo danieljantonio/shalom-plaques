ba4f12c74c34cb4000ca871fc5929a14936f4e99
Daniel J. Antonio <djedidiaha@gmail.com>
2022-08-03 01:52:58 +0700

    card component style now use gradient opacity

--- src/components/card.components.tsx
+++ src/components/card.components.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Card = ({ product }: { product: IProduct }) => {
 	return (
-		<div className='card m-4 mx-auto w-96 bg-base-100 shadow-lg'>
+		<div className='card m-4 mx-auto w-80 bg-base-100 shadow-lg'>
 			<figure>
 				<img src='https://placeimg.com/400/225/arch' className='h-56' alt='Shoes' />
 			</figure>
-			<div className='card-body'>
+			<div className='text-white card-body p-4 bg-gradient-to-t from-black absolute -right-0 w-full bottom-0'>
 				<h2 className='card-title'>{product.series}</h2>
 				<p>{product.description}</p>
 				<div className='card-actions justify-end'>
-					<div className='badge badge-outline badge-primary'>{product.subCategory.name}</div>
+					<div className='badge badge-outline'>{product.subCategory.name}</div>
 				</div>
 			</div>
 		</div>
