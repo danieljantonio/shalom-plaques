aa57014f5b46d55c4860e0038c3effeb444d56f9
Daniel J. Antonio <djedidiaha@gmail.com>
2022-08-10 13:21:52 +0700

    simplify getStaticProps into common util function

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps, NextPage } from 'next';
 import React from 'react';
 import ProductCatalogue from '../components/product-catalogue.components';
 import ProductHero from '../components/producthero.components';
+import { fetchData } from '../utils/fetch.utils';
 
 type Props = {
 	categories: ICategory[];
@@ -19,27 +20,6 @@ const Products: NextPage<Props> = ({ categories, subCategories, products }) => {
 	);
 };
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
-	try {
-		const { categories } = await fetch('http://localhost:5000/category').then((data) => data.json());
-		const { subCategories } = await fetch('http://localhost:5000/subcategory').then((data) => data.json());
-		const { products } = await fetch('http://localhost:5000/product').then((data) => data.json());
-		return {
-			props: {
-				categories: categories,
-				subCategories: subCategories,
-				products: products,
-			},
-		};
-	} catch (error) {
-		return {
-			props: {
-				categories: [],
-				subCategories: [],
-				products: [],
-			},
-		};
-	}
-};
+export const getStaticProps: GetStaticProps = async (ctx) => fetchData();
 
 export default Products;
