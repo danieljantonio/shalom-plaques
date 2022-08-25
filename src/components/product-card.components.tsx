31abbf014ea17f961f721a1ca6f3e3df8d4bf4a4
Daniel Antonio <djedidiaha@gmail.com>
2022-08-25 14:18:48 +0700

    add border to card component

--- src/components/product-card.components.tsx
+++ src/components/product-card.components.tsx
@@ -15,7 +15,7 @@ const Card: React.FC<Props> = ({ product, onClick }) => {
 	const imageUrl = product.images.length > 0 ? getProductImageUrls(product)[0] : 'https://placeimg.com/400/225/arch';
 
 	return (
-		<div className='card m-4 mx-auto w-4/5 md:w-80 bg-base-100 shadow-lg hover:cursor-pointer hover:shadow-2xl hover:scale-105 transition-all h-60' onClick={onClick}>
+		<div className='card border m-4 mx-auto w-4/5 md:w-80 bg-base-100 shadow-lg hover:cursor-pointer hover:shadow-2xl hover:scale-105 transition-all h-60' onClick={onClick}>
 			<Image src={`${imageUrl}`} className='object-cover md:w-full h-56' alt={product.series} layout='fill'></Image>
 			<div className='text-white card-body p-4 bg-gradient-to-t from-black absolute -right-0 w-full bottom-0'>
 				<h2 className='card-title'>{product.series}</h2>
