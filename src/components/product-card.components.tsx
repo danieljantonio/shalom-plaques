288e7e5b23511773e27d67628f1be2e90757de48
Daniel Antonio <djedidiaha@gmail.com>
2022-08-25 11:33:31 +0700

    product card shows first image if it exists

--- src/components/product-card.components.tsx
+++ src/components/product-card.components.tsx
@@ -10,11 +10,18 @@ type Props = {
 	onClick: () => void;
 };
 
-const Card: React.FC<Props> = ({ product, onClick }) => {
+const Card: React.FC<Props> = ({ product, onClick: _onClick }) => {
+	const onClick = () => {
+		console.log(product.images);
+		_onClick();
+	};
+
+	const imageUrl = product.images.length > 0 ? `${process.env.API_URL}/${product.images[0]}` : 'https://placeimg.com/400/225/arch';
+
 	return (
 		<div className='card m-4 mx-auto w-4/5 md:w-80 bg-base-100 shadow-lg hover:cursor-pointer hover:shadow-2xl hover:scale-105 transition-all h-fit' onClick={onClick}>
 			<figure>
-				<img src='https://placeimg.com/400/225/arch' className='object-cover md:w-full h-56' alt='Shoes' />
+				<img src={`${imageUrl}`} className='object-cover md:w-full h-56' alt='Shoes' />
 			</figure>
 			<div className='text-white card-body p-4 bg-gradient-to-t from-black absolute -right-0 w-full bottom-0'>
 				<h2 className='card-title'>{product.series}</h2>
