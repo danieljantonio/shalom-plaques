e05a120b6d2ae64b2df52f36dcac74d5bcca6e22
Daniel Antonio <djedidiaha@gmail.com>
2022-08-25 14:15:08 +0700

    use next/image for product card image

--- src/components/product-card.components.tsx
+++ src/components/product-card.components.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
 import { getProductImageUrls } from '../lib/products';
 /* 
@@ -15,13 +15,11 @@ const Card: React.FC<Props> = ({ product, onClick }) => {
 	const imageUrl = product.images.length > 0 ? getProductImageUrls(product)[0] : 'https://placeimg.com/400/225/arch';
 
 	return (
-		<div className='card m-4 mx-auto w-4/5 md:w-80 bg-base-100 shadow-lg hover:cursor-pointer hover:shadow-2xl hover:scale-105 transition-all h-fit' onClick={onClick}>
-			<figure>
-				<img src={`${imageUrl}`} className='object-cover md:w-full h-56' alt='Shoes' />
-			</figure>
+		<div className='card m-4 mx-auto w-4/5 md:w-80 bg-base-100 shadow-lg hover:cursor-pointer hover:shadow-2xl hover:scale-105 transition-all h-60' onClick={onClick}>
+			<Image src={`${imageUrl}`} className='object-cover md:w-full h-56' alt={product.series} layout='fill'></Image>
 			<div className='text-white card-body p-4 bg-gradient-to-t from-black absolute -right-0 w-full bottom-0'>
 				<h2 className='card-title'>{product.series}</h2>
-				<p>{product.description}</p>
+				{/* <p>{product.description}</p> */}
 				<div className='card-actions justify-end'>
 					<div className='badge badge-outline'>{product.subCategory.name}</div>
 				</div>
