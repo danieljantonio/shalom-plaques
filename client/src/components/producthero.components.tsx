f2a46a33daa2c65be9a61c53a75216547a5b575b
Daniel J. Antonio <djedidiaha@gmail.com>
2022-09-15 07:41:30 +0700

    fix product hero on smaller sites

--- client/src/components/producthero.components.tsx
+++ client/src/components/producthero.components.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 const ProductHero = () => {
 	return (
-		<div className='flex flex-col-reverse  mdl:h-1/2 mdl:flex-row justify-between bg-gradient-to-r from-yellow-800 to-orange-200'>
+		<div className='flex flex-col mdl:h-1/2 sm:flex-row justify-between bg-gradient-to-r from-yellow-800 to-orange-200'>
 			<div className='basis2/3 m-auto font-serif text-white'>
 				<div className='w-fit h-full text-center p-1/10 font-serif '>
-					<div className='font-semibold text-6xl text-center sm:text-8xl pb-5 underline'>
+					<div className='font-semibold text-6xl text-center md:text-8xl pb-5 underline'>
 						Product
 						<br />
 						Catalogue
@@ -15,7 +15,7 @@ const ProductHero = () => {
 				</div>
 			</div>
 			<div className='basis-1/3 h-full w-full m-auto'>
-				<Image src='/images/product/product-banner-1.png' layout='responsive' width={1620} height={1080}></Image>
+				<Image src='/images/product/product-banner-1.png' layout='responsive' width={1620} height={1080} className="sm:w-4/5"></Image>
 			</div>
 		</div>
 	);
