02fa016a34df390fe1e579f819167054a31e503a
Daniel J. Antonio <djedidiaha@gmail.com>
2022-09-15 09:35:34 +0700

    fix modal responsiveness on smaller screens

--- client/src/components/modal.components.tsx
+++ client/src/components/modal.components.tsx
@@ -34,36 +34,46 @@ const ProductModal: React.FC<Props> = ({ product, showModal, closeModal }) => {
 	const photos = getCollageImageUrls(product);
 
 	const collageSetting = {
-		width: '650px',
-		height: ['450px', '200px'],
+		width: '450px',
+		height: ['400px', '200px'],
 		layout: [1, 2],
 		showNumOfRemainingPhotos: true,
 		photos: photos,
 		style: { padding: '4px', backgroundSize: 'contain' },
 	};
 	const collageSettingSmall = {
-		height: ['350px', '150px'],
+		height: ['300px', '150px'],
 		layout: [1, 2],
 		showNumOfRemainingPhotos: true,
 		photos: photos,
-		style: { padding: '4px', backgroundSize: 'contain' },
+		style: { backgroundSize: 'contain' },
 	};
 
 	return (
 		<>
 			<Modal isOpen={showModal} shouldCloseOnOverlayClick={true} onRequestClose={closeModal} style={modalStyle}>
-				<div id='modal-content' className='flex flex-col lg:flex-row justify-between h-full'>
-					<div className='h-full w-1/2 items-center lg:pr-5 justify-center hidden lg:flex'>{photos.length > 0 ? <ReactPhotoCollage {...collageSetting} /> : <>No Image Found</>}</div>
-					<div className='w-full h-4/6 flex items-center lg:pr-5 justify-center lg:hidden'>{photos.length > 0 ? <ReactPhotoCollage {...collageSettingSmall} /> : <>No Image Found</>}</div>
-					<div className='w-full h-2/6 lg:w-1/2 pl-5 lg:h-full lg:border-l-2'>
+				<div id='modal-content' className='flex flex-col lg:flex-row justify-between pt-6 pb-4'>
+					{photos.length > 0 ? (
+						<>
+							<div className='h-full m-auto w-1/2 items-center lg:pr-5 justify-center hidden lg:flex'>
+								<ReactPhotoCollage {...collageSetting} />
+							</div>
+							<div className='w-4/5 mx-auto h-4/6 flex items-center lg:pr-5 justify-center lg:hidden'>
+								<ReactPhotoCollage {...collageSettingSmall} />
+							</div>
+						</>
+					) : (
+						<div>No Image Found</div>
+					)}
+					<div className='w-full h-2/6 lg:w-1/2 lg:pl-5 lg:h-full lg:border-l-2'>
 						<div className='lg:my-20'>
-							<h1 className='text-6xl font-bold mb-4'>{product.series}</h1>
-							<div className='pl-2'>
-								<div className='absolute bottom-6 right-6 flex space-x-4'>
+							<h1 className='text-2xl lg:text-6xl font-bold mb-4 pt-16'>{product.series}</h1>
+							<div className='lg:pl-2'>
+								<div className='italic text-gray-500'>{product.description ? product.description : 'No description added.'}</div>
+								<div className='mt-6 lg:absolute bottom-6 right-6 flex space-x-4'>
 									<div className='border border-orange-600 rounded-full px-4 py-1'>{product.category.name}</div>
 									<div className='border border-orange-600 rounded-full px-4 py-1'>{product.subCategory.name}</div>
 								</div>
-								<div className='italic text-gray-500'>{product.description ? product.description : 'No description added.'}</div>
 							</div>
 						</div>
 					</div>
