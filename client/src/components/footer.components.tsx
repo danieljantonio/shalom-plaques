c6a60b18961056526db71bb596823bf7953715a4
Daniel J. Antonio <djedidiaha@gmail.com>
2022-09-15 09:35:12 +0700

    minor changes to footer font

--- client/src/components/footer.components.tsx
+++ client/src/components/footer.components.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
 	return (
-		<footer className='w-full border-t-2 p-4 text-sm flex justify-between font-serif'>
+		<footer className='w-full border-t-2 p-4 text-sm flex justify-between font-serif mt-5'>
 			<div>Copyright © {new Date().getFullYear()} Shalom Plaques, Inc. All rights reserved.</div>
 			<div></div>
 		</footer>
