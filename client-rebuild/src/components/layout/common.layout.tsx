09e5bc7d83cfba7809dda57d62ad701f818e5e7b
Daniel J. Antonio <djedidiaha@gmail.com>
2023-03-08 07:58:59 +0700

    icon brand

--- client-rebuild/src/components/layout/common.layout.tsx
+++ client-rebuild/src/components/layout/common.layout.tsx
@@ -7,6 +7,12 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
     <div className="min-h-screen">
       <Navbar fluid={true}>
         <Navbar.Brand href="/">
+          <img
+            src="icons/favicon.png"
+            className="mr-3 h-6 sm:h-9"
+            alt="Shalom Plaques Logo"
+          />
+
           <span className="self-center whitespace-nowrap text-xl font-semibold">
             Shalom Plaques
           </span>
