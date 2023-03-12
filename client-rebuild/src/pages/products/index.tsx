93ac26a6f4f909e6e91d2eee01fe869524debf8e
Daniel J. Antonio <djedidiaha@gmail.com>
2023-03-12 22:04:51 +0700

    mock sidebar design

--- client-rebuild/src/pages/products/index.tsx
+++ client-rebuild/src/pages/products/index.tsx
@@ -1,5 +1,6 @@
 import { Button, Sidebar, Spinner } from "flowbite-react";
 import { type NextPage } from "next";
+import Link from "next/link";
 import MetaTag from "~/components/common/meta.common";
 import { api } from "~/utils/api";
 
@@ -8,9 +9,11 @@ const Products: NextPage = () => {
 
   const mapData = () => {
     if (!data) return;
-    for (const entries of data.entries()) {
-      console.log(entries[0]);
-    }
+    console.log(data);
+
+    // for (const entries of data.entries()) {
+    //   console.log(entries[0]);
+    // }
   };
 
   return (
@@ -20,7 +23,7 @@ const Products: NextPage = () => {
           selected woods."
       />
       <div style={{ height: 350 }} className="relative">
-        <div className="absolute z-10 h-full w-full bg-gradient-to-r from-pink-500 to-yellow-500 opacity-95"></div>
+        <div className="absolute z-10 h-full w-full bg-gradient-to-br from-pink-500 to-yellow-500 opacity-95"></div>
         <div className="absolute z-20 flex h-full w-full flex-col items-center justify-center font-serif text-white">
           <div className="space-y-3 text-center">
             <span className="text-2xl">Shalom Plaques</span>
@@ -31,34 +34,27 @@ const Products: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="mx-6 my-10 flex ">
+      <div className="my-12 flex">
         <div className="w-1/4">
-          <div className="w-fit rounded border">
-            <Sidebar>
-              <Sidebar.Items>
-                {isLoading ? (
-                  <div className="flex h-20 w-full items-center justify-center">
-                    <Spinner />
-                  </div>
-                ) : (
-                  <Sidebar.ItemGroup>
-                    <Sidebar.Collapse label={`Test`}>
-                      <Sidebar.Item>Content 1</Sidebar.Item>
-                      <Sidebar.Item>Content 2</Sidebar.Item>
-                      <Sidebar.Item>Content 3</Sidebar.Item>
-                    </Sidebar.Collapse>
-                    <Sidebar.Collapse label={`Test`}>
-                      <Sidebar.Item>Content 1</Sidebar.Item>
-                      <Sidebar.Item>Content 2</Sidebar.Item>
-                      <Sidebar.Item>Content 3</Sidebar.Item>
-                    </Sidebar.Collapse>
-                  </Sidebar.ItemGroup>
-                )}
-              </Sidebar.Items>
-            </Sidebar>
+          <div className="mx-auto flex min-h-[50] w-4/5 flex-col space-y-2 rounded ">
+            <Link
+              href="#"
+              className="overflow-hidden rounded border bg-yellow-400 px-6 py-4 text-white hover:shadow-md"
+            >
+              Test (Active)
+            </Link>
+            <Link href="#" className="rounded border px-6 py-4 hover:shadow-md">
+              Test
+            </Link>
+            <Link href="#" className="border px-6 py-4 hover:shadow-md">
+              Test
+            </Link>
+            <Link href="#" className="border px-6 py-4 hover:shadow-md">
+              Test
+            </Link>
           </div>
         </div>
-        <div className="w-3/4">
+        <div className="w-full">
           <Button onClick={mapData}>Log</Button>
         </div>
       </div>
